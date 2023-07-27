a621759246b5fa1a8e6f35417ab770aef2723a56
xmrui <17803821620@163.com>
2023-07-28 00:04:28 +0800

    first

---
+++ src/test/java/com/xmrui/start/DreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmrui.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
