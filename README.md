63acb7e3c70da1895ceb37a9ceb3e0f90c84bd30
xmrui <17803821620@163.com>
2023-08-13 22:13:42 +0800

    现在都用idea的md文件写笔记

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# dream
-1
+# note目录是学习笔记
+## dream
+### dream 
+#### dream 
+##### dream
+###### dream
---
+++ src/main/java/jproject/Pc.java
@@ -0,0 +1,7 @@
+package jproject;
+
+public class Pc {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
